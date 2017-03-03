e48688d6f19c4a318bcccf921793b63e02bbd3b3
Sailesh Patel <saileshapatel@gmail.com>
2017-03-03 19:55:41 +0000

    added comments

--- create.sh
+++ create.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# Authors: Matt Barlow, Sailesh Patel
+# Purpose: Create a plain bash script with permissions
+
+# get variables
 read -p "Enter a filename: " n
+# create a new .sh file
 touch $n.sh
+# give the .sh file permissions
 chmod u+x $n.sh
+# put heading in .sh file
 echo "#!/bin/bash" >> $n.sh
+# opens file in nano
 nano $n.sh
